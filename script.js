2f65db82330d17aba15d8ec15841e791eae8b1eb
satyam-kumar-verma <billionairedolike@gmail.com>
2023-10-21 20:17:52 +0530

    change

--- script.js
+++ script.js
@@ -1,80 +1,113 @@
-let inputEl = document.getElementById("searchInput");
-let bottomHeadingEl = document.getElementById("bottomHeading");
-
-let resultContainerEl = document.getElementById("searchResults");
+let inputEl = document.getElementById("inputEl");
+let resultText = document.getElementById("resultText");
 
 let loading = document.getElementById("spinner");
 
-function createAndAppendResults(eachItem) {
-    let bookAndTitleContainer = document.createElement("div");
-    bookAndTitleContainer.classList.add("mr-auto", "ml-auto");
-    resultContainerEl.appendChild(bookAndTitleContainer);
+let resultContainer = document.getElementById("resultContainer");
+
+let randomColor;
+
+function generateRandomColor(){
+    let red = Math.ceil(Math.random()*255);
+    let green = Math.ceil(Math.random()*255);
+    let blue = Math.ceil(Math.random()*255);
+    
+    randomColor = `rgb(${red},${green},${blue})`;
+}
+
+
+function createAndAppendBookStyle(eachItem){
+
+    generateRandomColor();
+
+    let detailContainer = document.createElement("div");
+    detailContainer.classList.add("col-5", "book-container", "mb-3");
+    detailContainer.style.backgroundColor = randomColor;
+    resultContainer.appendChild(detailContainer);
 
     let imageEl = document.createElement("img");
-    imageEl.src = eachItem.imageLink;
-    imageEl.classList.add("book-img-style");
-    bookAndTitleContainer.appendChild(imageEl);
+    imageEl.classList.add("img-style");
+    imageEl.src= eachItem.imageLink;
+    detailContainer.appendChild(imageEl);
 
-    let authorName = document.createElement("p");
+    let nameAndAuthorDetailContainer = document.createElement("div");
+    nameAndAuthorDetailContainer.classList.add("detail-container");
+    detailContainer.appendChild(nameAndAuthorDetailContainer);
+
+    let authorEl = document.createElement("p");
+    authorEl.textContent = "Author: ";
+    nameAndAuthorDetailContainer.appendChild(authorEl);
+
+    let authorName = document.createElement("span");
+    authorName.classList.add("detail-style");
     authorName.textContent = eachItem.author;
-    authorName.classList.add("title-style");
-    bookAndTitleContainer.appendChild(authorName);
+    authorEl.appendChild(authorName);
+
+    let bookEl = document.createElement("p");
+    bookEl.textContent = "Book name: ";
+    nameAndAuthorDetailContainer.appendChild(bookEl);
+
+    let bookName = document.createElement("span");
+    bookName.classList.add("detail-style");
+    bookName.textContent = eachItem.title;
+    bookEl.appendChild(bookName);
 
 }
 
-function sendAllData(search_results) {
-    resultContainerEl.textContent = "";
+function getAllData(search_results){
 
-    for (let eachItem of search_results) {
-        createAndAppendResults(eachItem);
+    resultText.textContent = "Popular Books";
+
+    for(let eachItem of search_results){
+        createAndAppendBookStyle(eachItem);
     }
 }
 
-function sendNotFoundData() {
-    bottomHeadingEl.textContent = "No result found";
-    bottomHeadingEl.classList.add("bottom-heading-center");
+function printNotFound(){
+    resultText.textContent = "No Book Found";
 }
 
-function makeCall(event) {
-    let url = "https://apis.ccbp.in/book-store?title=" + inputEl.value;
-
-    if (event.key === "Enter") {
-
-        if (inputEl.value === "") {
-            alert("input is empty");
-            return true;
-        }
+function fetchData(event){
+    if(event.key === "Enter"){
 
         loading.classList.toggle("d-none");
-        resultContainerEl.textContent = "";
-        bottomHeadingEl.textContent = "";
-        bottomHeadingEl.classList.remove("bottom-heading-center");
+        resultContainer.textContent = "";
+        resultText.textContent = "";
+
+        let inputValue = event.target.value;
+
+        if(inputValue === ""){
+            alert("Please write something in searchbox");
+            return true;
+        }
 
         let options = {
             method: "GET"
         };
 
+        let url = "https://apis.ccbp.in/book-store?title=" + inputValue;
+
         fetch(url, options)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(jsonData) {
-                console.log(jsonData);
-                let {
-                    search_results
-                } = jsonData;
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(jsonData){
+            console.log(jsonData);
 
-                loading.classList.toggle("d-none");
+            loading.classList.toggle("d-none");
 
-                if (search_results.length === 0) {
-                    sendNotFoundData();
-                } else {
-                    bottomHeadingEl.textContent = "Popular Books";
-                    sendAllData(search_results);
-                }
-            });
+            let {search_results} = jsonData;
+
+            if(search_results.length === 0){
+                printNotFound();
+                return true;
+            }
+            else{
+                getAllData(search_results);
+            }
+
+        })
     }
-
 }
 
-inputEl.addEventListener("keydown", makeCall);
+inputEl.addEventListener("keydown",fetchData);
